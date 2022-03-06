7d91e1ec7802e1bba8a00874ba568f59c8c059a9
saqibrazzaq <saqibrazzaq@gmail.com>
2022-03-06 09:48:14 +0500

    added repository

--- src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
+++ src/Services/Basket/Basket.API/Repositories/IBasketRepository.cs
@@ -4,8 +4,8 @@ namespace Basket.API.Repositories
 {
     public interface IBasketRepository
     {
-        Task<ShoppingCart> GetBasket(string userName);
-        Task<ShoppingCart> UpdateBasket(ShoppingCart basket);
+        Task<ShoppingCart?> GetBasket(string userName);
+        Task<ShoppingCart?> UpdateBasket(ShoppingCart basket);
         Task DeleteBasket(string userName);
     }
 }
